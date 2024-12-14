b5138a9e24ed7b397584341a74b5a27514e98264
vishalgarna <vishalgarna93@gmail.com>
2024-12-14 13:08:58 +0530

    update sotring function of data whoose storing null values

--- ForexData.services.js
+++ ForexData.services.js
@@ -17,35 +17,68 @@ async function addPair(params) {
             // new data seave karo with symbol 
             if (CartDB === null) {
 
-                if (params.symbol != null) {
-                    const newdata = {
-                        "symbol": params.symbol,
-                        "data1m": [
-                            params.data
-                        ]
+
+                if (params.timeframe === '1m') {
+
+
+                    if (params.symbol != null && params.close != null) {
+                        const newdata = {
+                            "symbol": params.symbol,
+                            "data1m": [
+                                params.data
+                            ]
+                        }
+                        const newForexPair = ForexDataModel(newdata)
+                        await newForexPair.save().then(() => console.log("succesfully added new pair ")).catch((err) => console.log(err))
                     }
-                    const newForexPair = ForexDataModel(newdata)
-                    await newForexPair.save().then(() => console.log("succesfully added new pair ")).catch((err) => console.log(err))
+                }
+
+                else if (params.timeframe === '5m') {
+
+                    if (params.symbol != null && params.close != null) {
+                        const newdata = {
+                            "symbol": params.symbol,
+                            "data5m": [
+                                params.data
+                            ]
+                        }
+                        const newForexPair = ForexDataModel(newdata)
+                        await newForexPair.save().then(() => console.log("succesfully added new pair ")).catch((err) => console.log(err))
+                    }
+                }
+
+                else {
+                    if (params.symbol != null && params.close != null) {
+                        const newdata = {
+                            "symbol": params.symbol,
+                            "data15m": [
+                                params.data
+                            ]
+                        }
+                        const newForexPair = ForexDataModel(newdata)
+                        await newForexPair.save().then(() => console.log("succesfully added new pair ")).catch((err) => console.log(err))
+                    }
+
                 }
             }
             // agar mil jata hai 
             else {
                 // yaha ye find karke update kar raha hai 
 
-                if (params.timeframe === '1m') {
+                if (params.timeframe === '1m' && params.close != null) {
                     await ForexDataModel.updateOne({ symbol: params.symbol }, { $push: { data1m: params.data } })
                         .then(() => console.log('update 1min data SuccessFully')
                         ).catch((err) => console.log('error in 1 min data saving')
                         )
                 }
-                else if (params.timeframe === '5m') {
+                else if (params.timeframe === '5m' && params.close != null) {
 
                     await ForexDataModel.updateOne({ symbol: params.symbol }, { $push: { data5m: params.data } })
                         .then(() => console.log('updated 5 min data successsfully')
                         ).catch((err) => console.log('error in 5 min data saving')
                         )
                 }
-                else if (params.timeframe === '15m') {
+                else if (params.timeframe === '15m' && params.close != null) {
 
                     await ForexDataModel.updateOne({ symbol: params.symbol }, { $push: { data15m: params.data } })
                         .catch((err) => console.log('error in 15 min data saving')
